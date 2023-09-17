a7178e5f94913c4d532d1401c0149a8d730313d3
payton99 <payton.fisher99@gmail.com>
2023-09-17 00:21:43 -0400

    Initiated OOP framework in president.py

--- president.py
+++ president.py
@@ -1,8 +1,56 @@
+import re
 import pandas as pd
 import numpy as np
+import requests
+from bs4 import BeautifulSoup
 
 # 0 for Republican
 # 1 for Democratic
+
+class President:
+
+    pres_url = "https://en.wikipedia.org/wiki/List_of_United_States_presidential_election_results_by_state"
+
+    def __init__(self):
+        pass
+
+    def status_code(self):
+        try:
+            r = requests.head(self.pres_url)
+            print(r.status_code)
+        except requests.ConnectionError:
+            print(f"** FAILURE **: Failed to connect to given url: {self.pres_url}")
+    
+    def get_table(self):
+        site = requests.get(self.pres_url)
+        soup = BeautifulSoup(site.text, 'html.parser')
+        table = soup.find('table', attrs={'class': 'wikitable'})
+        return table
+
+
+
+def get_data(url):
+    site = requests.get(url)
+    soup = BeautifulSoup(site.text, 'html.parser')
+    table = soup.find('table', attrs={'class': 'wikitable'})
+    years = set()
+    date = '\d{4}'
+    for x in table.find_all('th'):
+        if re.search(date, x.text.strip()):
+            years.add(x.text.strip())
+    print(years)
+    tr = table.find_all('tr')
+    vals = []
+    # for row in tr:
+        # print(row.text.strip())
+    #     headers = row.find_all('td')
+        # cols = row.find_all('td')
+        # print(cols)
+    # print(headers)
+        # t_row = [row.text for row in cols]
+        # print(t_row)
+        # vals.append(t_row)
+    # return vals
 
 def load_csv():
     df = pd.read_csv("presidential-elections.csv")
@@ -28,4 +76,5 @@ def percent_from_half():
 if __name__ == '__main__':
     # print(load_csv())
     # print(sum_across())
-    print(percent_from_half())
+    pres = President()
+    pres.get_table()
